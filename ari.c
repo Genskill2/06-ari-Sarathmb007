d886eb8623596423154206a7f30a5f1e4726aa9c
Sarathmb007 <84663624+Sarathmb007@users.noreply.github.com>
2021-06-29 13:32:17 +0530

    Done

---
+++ ari.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <cs50.h>
+
+
+string ari(string s){
+float characters=0;
+float words=0;
+float sentences=0;
+for(int i=0;i<strlen(s);i++){
+if(isalnum(s[i])){
+characters+=1;
+}else if(s[i]==' '){
+words+=1;
+}else if(s[i]=='.' || s[i]=='!' || s[i]=='?'){
+sentences+=1;
+}
+}
+int index;
+index= 4.71*characters/words+0.5*words/sentences-21.43;
+index+=1;
+if(index==1){return "Kindergarden";}
+if(index==2){return "First/Second Grade";}
+if(index==3){return "Third Grade";}
+if(index==4){return "Fourth Grade";}
+if(index==5){return "Fifth Grade";}
+if(index==6){return "Sixth Grade";}
+if(index==7){return "Seventh Grade";}
+if(index==8){return "Eighth Grade";}
+if(index==9){return "Ninth Grade";}
+if(index==10){return "Tenth Grade";}
+if(index==11){return "Eleventh Grade";}
+if(index==12){return "Twelfth grade";}
+if(index==13){return "College student";}
+if(index>=14){return "Professor";}
+}
